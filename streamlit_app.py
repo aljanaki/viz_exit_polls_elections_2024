c2278774e3cd401524c228823aa193201005aedc
aljanaki <aljanaki@gmail.com>
2024-03-19 15:15:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,51 +1,88 @@
+#######################
+# Import libraries
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import altair as alt
 
-
+#######################
+# Page configuration
 st.set_page_config(
-    page_title="Exit Poll Statistics for Russian elections 2024: Voting Abroad",
+    page_title="Экзитполы 2024 по российским выборам за рубежом",
     layout="wide",
     initial_sidebar_state="expanded")
 
-alt.themes.enable("dark")
 
+#######################
+# Load data
 df = pd.read_csv('data_exit_polls.csv')
 
 #######################
+# Sidebar
 with st.sidebar:
-    st.title('Vote statistics options')
-    option_list = ['percent_putin', 'percent_declined',
-       'percent_voted_putin_and_declined', 'percent_spoiled']
+    st.title('Опции визуализации')
 
-    selected_option = st.selectbox('Select an option', option_list)
+    option_list = ["Испорченный бюллетень (%)", "Путин (%)", "Не хочу отвечать (%)", "Даванков (%)", "Слуцкий (%)", "Харитонов (%)"]
+    option_list_column_names = ['percent_spoiled', 'percent_putin', 'percent_declined',
+       'davankov_perc', 'slutskiy_perc', 'haritonov_perc']
+
+    selected_option = st.selectbox('Выбери статистику для визуализации', option_list)
+
 #######################
+# Plots
+def createHoverTexts(df):
+    '''
+    Auxilliary function, creates hover text for each country with sorted statistics
+    :param df:
+    :return:
+    '''
+    df['Hovertext'] = ''
+    for index, row in df.iterrows():
+        dic = {'Даванков': row['davankov_perc'], "Слуцкий": row['slutskiy_perc'], "Харитонов": row['haritonov_perc'],
+               "Путин": row['percent_putin'], 'Испорчено': row['percent_spoiled'],
+               "Отказались отвечать": row['percent_declined']}
+        sortedDict = dict(sorted(dic.items(), key=lambda item: item[1], reverse=True))
+        texts = []
+        texts.append(row['Country_rus'])
 
-def make_choropleth(input_df, selected_option):
+        for item in sortedDict.items():
+            texts.append(f"{item[0]}: {str(item[1])}%")
+        texts.append(f"Опрошено человек: {row['answers_collected']}")
+        hovertext = '<br>'.join(texts)
+        df.at[index, 'Hovertext'] = hovertext
+    return df
+
+
+# Choropleth map
+def make_choropleth(input_df, selected_option, option_list, option_list_column_names):
+    input_df = createHoverTexts(input_df)
+
     choropleth = px.choropleth(input_df, locations="iso_alpha",
-                  color=selected_option,
-                  hover_name="Country",
-                  color_continuous_scale=px.colors.sequential.Reds,
-                  projection='robinson')
+                  color= option_list_column_names[option_list.index(selected_option)],
+                  hover_name="Hovertext",
+                  color_continuous_scale=px.colors.sequential.Blues,
+                  projection='robinson',
+                 hover_data={option_list_column_names[option_list.index(selected_option)]: False, 'iso_alpha': False})
+
     choropleth.update_layout(
-        template='plotly_dark',
-        plot_bgcolor='rgba(0, 0, 0, 0)',
-        paper_bgcolor='rgba(0, 0, 0, 0)',
         margin=dict(l=0, r=0, t=0, b=0),
-        height=350
+        width = 800,
+        height = 800
     )
+
     return choropleth
 
+st.markdown('''### Результаты экзит полов
 
-st.markdown('Exit poll results')
 
-choropleth = make_choropleth(df, selected_option)
+Наведите курсор на страну, чтобы увидеть подробности. 
+''')
+
+choropleth = make_choropleth(df, selected_option, option_list, option_list_column_names)
 st.plotly_chart(choropleth, use_container_width=True)
 
 
-with st.expander('About', expanded=True):
+with st.expander('Источник данных', expanded=True):
     st.write('''
-            - Data: Экзитполлы "Голосуй за рубежом" 
+            Экзитполлы "Голосуй за рубежом" 
             https://docs.google.com/spreadsheets/d/1HwIbQLXxcB0GAc8DA6uIkYNj0BCXhROKUp7kto9k8Z0/edit#gid=373097824
             ''')
